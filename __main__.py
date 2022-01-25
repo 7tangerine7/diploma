b91c0e7fdd26816a373a22e346a2ad00db0c09d0
Polina <ptukhap@gmail.com>
2022-01-25 23:28:11 +0200

    move indexes

--- __main__.py
+++ __main__.py
@@ -13,22 +13,22 @@ def alg():
     H[:, 0, 0] = weight[:, 0, 0, 0]
     d = np.zeros((row, col), dtype=int)
     for i in range(1, col-1):
-        k = np.fromfunction(lambda r, y: weight[r, i, y, d[r, i]], (row, D), dtype=int)
+        k = np.fromfunction(lambda r, y: weight[r, i, y, d[r, i-1]], (row, D), dtype=int)
         H[:, i, :] = np.where(H[:, i, :] != np.inf, k, np.inf)
-        d[:, i+1] = np.argmin(H[:, i, :], axis=1)
-    H[:, col-1, :] = np.where(H[:, col-1, :] != np.inf, weight[:, col-1, :, d[:, i]][3], np.inf)
+        d[:, i] = np.argmin(H[:, i, :], axis=1)
+    H[:, col-1, :] = np.where(H[:, col-1, :] != np.inf, weight[:, col-1, :, d[:, col-2]][3], np.inf)
     final = (d - np.min(d))/(np.max(d) - np.min(d))
     cv2.namedWindow("final")
     cv2.imshow("final", final)
     cv2.waitKey(0)
 
-D = 11
-alpha = 3
-file_path_r = Path("im0.ppm").resolve()
+D = 10
+alpha = 0.9
+file_path_r = Path("scene1.row3.col1.ppm").resolve()
 left = cv2.imread(str(file_path_r), cv2.IMREAD_GRAYSCALE)
 row, col = left.shape[:2]
 
-file_path_l = Path("im2.ppm")
+file_path_l = Path("scene1.row3.col2.ppm")
 right = cv2.imread(str(file_path_l), cv2.IMREAD_GRAYSCALE)
 
 alg()
